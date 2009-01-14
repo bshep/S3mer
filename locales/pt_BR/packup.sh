c0f25585a79548528a38accda753a36930bba93c
bruce <bruce@97ee0192-8d47-0410-938a-d224e5126147>
2009-01-14 05:18:56 +0000

    - Adding support for localization of app, included is pt_BR
    git-svn-id: http://svns3mer/svn/s3mer/player/trunk@909 97ee0192-8d47-0410-938a-d224e5126147

---
+++ locales/pt_BR/packup.sh
@@ -0,0 +1,18 @@
+DEST=/Applications/Adobe\ Flex\ Builder\ 3/sdks/3.2.0/frameworks/locale
+LOCALE=pt_BR
+
+pushd airframework_rb_orig
+zip -r "$DEST/$LOCALE/airframework_rb.swc" * -x *.DS_Store
+popd
+
+pushd automation_rb_orig
+zip -r "$DEST/$LOCALE/automation_rb.swc" * -x *.DS_Store
+popd
+
+pushd framework_rb_orig
+zip -r "$DEST/$LOCALE/framework_rb.swc" * -x *.DS_Store
+popd
+
+pushd rpc_rb_orig
+zip -r "$DEST/$LOCALE/rpc_rb.swc" * -x *.DS_Store
+popd
