39abc67d18377627b8d3a96e2c3da23c469940d4
bruce <bruce@97ee0192-8d47-0410-938a-d224e5126147>
2009-01-14 14:01:11 +0000

    - Update crealocales to work independently and also to build all locales automatically - Set version back to .991 since we were testing auto update
    git-svn-id: http://svns3mer/svn/s3mer/player/trunk@912 97ee0192-8d47-0410-938a-d224e5126147

--- S3mer/createLocales.sh
+++ S3mer/createLocales.sh
@@ -1,5 +1,34 @@
 #!/bin/sh
 
-pushd ../locales/pt_BR
-./packup.sh
-popd
+
+DEST=/Applications/Adobe\ Flex\ Builder\ 3/sdks/3.2.0/frameworks/locale
+
+LOCALES_DIR=../locales
+
+LOCALES_LIST=`ls $LOCALES_DIR`
+LOCALES_FILES="airframework_rb automation_rb framework_rb rpc_rb"
+
+# echo $LOCALES_LIST
+
+for locale in $LOCALES_LIST; do
+	pushd "$LOCALES_DIR/$locale" > /dev/null
+	
+	echo $locale
+	for locale_file in $LOCALES_FILES; do
+		locale_dir=${locale_file}_orig
+		if [[ -d "${locale_dir}" ]]; then
+			mkdir -p "$DEST/$locale"
+			
+			pushd ${locale_dir} > /dev/null
+			
+			echo "-" "${locale_dir}"
+			
+			rm "${DEST}/${locale}/${locale_file}.swc"
+			zip -r "${DEST}/${locale}/${locale_file}.swc" * -x *.DS_Store *.svn*		
+			
+			popd > /dev/null
+		fi
+	done
+	
+	popd > /dev/null
+done
